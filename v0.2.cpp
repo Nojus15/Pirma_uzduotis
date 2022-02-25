2007af0a7eb9da1cc65b88767b01cc3598f2e4e5
Nojus <53999653+Nojus15@users.noreply.github.com>
2022-02-25 11:10:40 +0200

    minor nd calc fixes

--- v0.2.cpp
+++ v0.2.cpp
@@ -149,20 +149,24 @@ double galutinisVid(vector<int> paz, int egz)
     {
         sum += el;
     }
-
-    return sum / (paz.size() * 1.0) * 0.4 + egz * 0.6;
+    if (paz.size() != 0)
+        return sum / (paz.size() * 1.0) * 0.4 + egz * 0.6;
+    else
+        return egz * 0.6;
 }
 double galutinisMed(vector<int> paz, int egz)
 {
     sort(paz.begin(), paz.end());
-    if (paz.size() % 2 == 0)
+    if (paz.size() % 2 == 0 && paz.size() != 0)
     {
         return (paz[paz.size() / 2] + paz[paz.size() / 2 - 1]) * 1.0 / 2;
     }
-    else
+    else if (paz.size() % 2 != 0 && paz.size() != 0)
     {
         return paz[paz.size() / 2] * 0.4 + egz * 0.6;
     }
+    else
+        return egz * 0.6;
 }
 int enterValidInt()
 {
